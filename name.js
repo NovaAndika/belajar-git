9ae7e527f90bc0736d7e2700291c5c9be5bf3d69
Nova Andika <new.novaandika@gmail.com>
2024-01-04 23:10:14 -1200

    Menambahkan feth api

---
+++ name.js
@@ -0,0 +1,17 @@
+function name(x, y){
+    return x + y
+}
+const tambah = name(20,39)
+console.log(tambah)
+
+const xml = new XMLHttpRequest();
+xml.open('GET', 'http://127.0.0.1/api/auth/register')
+xml.onload = () => {
+    if (xml.status === 200){
+        const data = JSON.parse(xml.responseText);
+        console.log(data)
+    }else {
+        console.error("Network Response was mot ok")
+    }
+}
+xml.send(); 
